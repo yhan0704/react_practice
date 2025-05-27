83f95a410c3987b255876ee1557735734ac8a36c
Young Chan Han <hanyc1205@gmail.com>
2025-05-27 16:34:51 -0400

    fetched and popoulated  todos

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,50 @@ type Todo = {
   completed: boolean;
 };
 
-function App() {}
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchedData = async () => {
+      setIsLoading(true);
+      try {
+        const fetchedTodos = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const getData = await fetchedTodos.json();
+        setTodos(getData.slice(0, 10));
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchedData();
+  }, []);
+
+  console.log("todos", todos);
+  console.log("isLoading", isLoading);
+  console.log("isError", isError);
+
+  if (isError) return <div>Error</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div style={{ margin: "auto", maxWidth: "700px", textAlign: "center" }}>
+      <h1>Todo List</h1>
+      {todos.map((todo) => (
+        <div style={{ textAlign: "start" }}>
+          <label>
+            <input type="checkbox" />
+            {todo.title}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
